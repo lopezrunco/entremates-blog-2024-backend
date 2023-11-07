a06fcbc27f986aaef811549bb8766e2975e63daa
membrillo amargo <lopezrunco@gmail.com>
2023-11-07 15:50:50 +0530

    User registration deleted (Only admin user allowed)

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+
 import logging from '../config/logging';
 import User from '../models/user';
-import mongoose from 'mongoose';
 
 const validate = (req: Request, res: Response, next: NextFunction) => {
     logging.info('Token validated, returning user...');
@@ -25,34 +25,6 @@ const validate = (req: Request, res: Response, next: NextFunction) => {
         });
 };
 
-const create = (req: Request, res: Response, next: NextFunction) => {
-    logging.info('Attempting to register the user...');
-    let { uid, name } = req.body;
-    let fire_token = res.locals.fire_token;
-
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        uid,
-        name
-    });
-
-    return user
-        .save()
-        .then((newUser) => {
-            logging.info(`New user ${uid} created.`);
-            return res.status(201).json({
-                user: newUser,
-                fire_token
-            });
-        })
-        .catch((error) => {
-            logging.error(error);
-            return res.status(500).json({
-                error
-            });
-        });
-};
-
 const login = (req: Request, res: Response, next: NextFunction) => {
     logging.info('Logging in user...');
     let { uid } = req.body;
@@ -67,8 +39,7 @@ const login = (req: Request, res: Response, next: NextFunction) => {
                     fire_token
                 });
             } else {
-                logging.info(`User ${uid} not found, register...`);
-                return create(req, res, next);
+                logging.info(`User ${uid} not found.`);
             }
         })
         .catch((error) => {
@@ -120,7 +91,6 @@ const readAll = (req: Request, res: Response, next: NextFunction) => {
 
 export default {
     validate,
-    create,
     login,
     read,
     readAll
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,13 @@
 import express from 'express';
+
 import controller from '../controllers/user';
 import extractFirebaseInfo from '../middleware/extractFirebaseInfo';
 
 const router = express.Router();
 
+router.post('/login', extractFirebaseInfo, controller.login);
 router.get('/validate', extractFirebaseInfo, controller.validate);
 router.get('/read/:userID', controller.read);
-router.post('/create', extractFirebaseInfo, controller.create);
-router.post('/login', extractFirebaseInfo, controller.login);
 router.get('/', controller.readAll);
 
 export = router;
